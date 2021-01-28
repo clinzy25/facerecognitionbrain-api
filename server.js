b71f72fb07d4a788e07cbd3d2b9c40e9624a6c0a
clinzy25 <conner.linzy25@gmail.com>
2021-01-28 10:54:41 -0500

    pre-deployment

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const cors = require('cors');
 const knex = require('knex');
 const { response } = require('express');
 
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 const db = knex({
     client: 'pg',
     connection: {
@@ -15,93 +20,21 @@ const db = knex({
     }
 });
 
-db.select('*').from('users').then(data => {
-    console.log(data);
-});
-
 const app = express();
-
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
-const database ={
-    users: [
-        {
-            id: '123',
-            name: 'john',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '1234',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
+app.get('/', (req, res) => { res.send('success'); })
 
-app.get('/', (req, res) => {
-    res.send(database.users);
-})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
-app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('error loggin in');
-    }
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res ,db, bcrypt) })
 
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    db('users')
-        .returning('*')
-        .insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    })
-    .then(user => {
-        res.json(user[0]);
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 
-app.get('/profile/:id', (req, res) => {
-    const{ id } = req.params;
-    db.select('*').from('users').where({id}) 
-        .then(user=> {
-        if (user.length) {
-            res.json(user[0])
-        } else {
-            res.status(400).json('error getting user');
-        }
-    })
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(3000, () => {
     console.log('app is running on 3000');
